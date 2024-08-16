0a33e0c3b929bd9b916182a03606594aa8a862cb
Dinesh Babu Vaddineni <89186228+dineshbabuvaddineni@users.noreply.github.com>
2024-08-17 00:25:41 +0530

    Add files via upload

---
+++ selenium_day22_TestNGAnnotationsAndTypesofAssertions/AnnotationsDemo2.java
@@ -0,0 +1,35 @@
+package selenium_day22_TestNGAnnotationsAndTypesofAssertions;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/*1)Login  ------> @BeforeClass
+2)Search   ------> @Test
+3)Adv Search ----> @Test
+4)Logout   ------->@AfterClass*/
+
+public class AnnotationsDemo2 {
+	
+  @BeforeClass
+  public void login() {
+	  System.out.println("This is login method");
+  }
+  
+  @AfterClass
+  public void logout() {
+	  System.out.println("This is logout method");
+	  
+  }  
+  @Test(priority=1)
+  void search() {
+	  System.out.println("This is search method");
+	  
+  }
+  
+  @Test(priority=2)
+  void advancedsearch() {
+	  System.out.println("This is advanced search method");
+	  
+  }
+}
---
+++ selenium_day22_TestNGAnnotationsAndTypesofAssertions/HardAssertions.java
@@ -0,0 +1,30 @@
+package selenium_day22_TestNGAnnotationsAndTypesofAssertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HardAssertions {
+  @Test
+  public void test() {
+	  //Assert.assertEquals("xyz", "xyz"); //passed
+	  //Assert.assertEquals(123, 345);     //Failed
+	  
+	  //Assert.assertEquals("xyz", 123);   //Failed
+	  //Assert.assertEquals("123", 123);   //Failed
+	  
+	  //Assert.assertNotEquals(123,123);    //failed
+	  //Assert.assertNotEquals(123,345);   //passed
+	  
+	  //Assert.assertTrue(true);  //pass
+	  //Assert.assertTrue(false); //failed
+	  
+	  // Assert.assertTrue(1==2);  //failed
+	  // Assert.assertTrue(1==1);  //passed
+	  
+	  //Assert.assertFalse(1==2); //passed
+	  //Assert.assertFalse(1==1); //failed
+	  
+	  Assert.fail();
+	    
+  }
+}
